40bed0e699744fa63b84f2524e85b904a550a1e4
yashaskor <ya1985.85@gmail.com>
2023-02-22 21:16:39 +0200

    feat: create and test GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.guessNumber = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessNumber = Math.round((this.min + this.max) / 2);
+        return this.guessNumber;
     }
 
     lower() {
-
+        this.max = this.guessNumber;
     }
 
     greater() {
-
+        this.min = this.guessNumber;
     }
 }
 
